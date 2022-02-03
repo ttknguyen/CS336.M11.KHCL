f9a88173afa8b6d767cc004f24edbaf6edf48ed8
JD981 <19520235@gm.uit.edu.vn>
2022-02-03 23:55:57 +0700

    update setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,14 @@
+# download data
 gdown --id 13dF7KYDqfu_Qd2Dqc8ZkJy8P-ZHLUAAS
 unzip dataset.zip
+rm dataset.zip
+
+# install librabries
 pip install -q torch==1.0.0 torchvision
+pip install flask-cors
+pip install flask-ngrok
+
+# install ngrok token
+curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz -o ngrok.tgz
+tar xvzf ngrok.tgz
+./ngrok authtoken 24Gj4z8JODNMriG0cCB9NfWTRzg_3zzBeRVQnynadewEHQDaG
