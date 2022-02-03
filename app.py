cd6e5d6efce154966187678c94ca4f25a470f1f2
JD981 <19520235@gm.uit.edu.vn>
2022-02-03 11:48:25 +0700

    add method to app.py

--- app.py
+++ app.py
@@ -5,10 +5,9 @@ import json
 import numpy as np
 from PIL import Image
 from datetime import datetime
-from flask import Flask, request, render_template
+from flask import Flask, request
 from pathlib import Path
 from flask_cors import CORS, cross_origin
-from flask_ngrok import run_with_ngrok
 from retrieval_model import load_features, load_corpus, method_0, method_1, method_2, load_methods
 
 app = Flask(__name__)
@@ -21,7 +20,7 @@ path_data = '/content/CS336.M11.KHCL/data/'
 path_corpus = '/content/CS336.M11.KHCL/data/test/oxford5k/jpg/'
 
 corpus = load_corpus(path_corpus)
-method0, method1, method2 = load_features(path_data, corpus)
+fe_method0, fe_method1, fe_method2 = load_features(path_data, path_corpus)
 model, delf = load_methods(root)
 
 
@@ -38,38 +37,38 @@ def encode_img(pathImg):
     base64str = base64str[2:-1]
     return base64str
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 @cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def index():
-    if request.method == 'POST':
-        # Upload image
-        query_file = request.form
-        # print(query_file)
+    # Upload image
+    query_file = request.form
+    base64Img = query_file['image']
+    x1, y1 = int(float(query_file['x'])), int(float(query_file['y']))
+    x2, y2 = int(float(query_file['x_max'])), int(float(query_file['y_max']))
+    methodRequest = query_file['method']
+    
+    # Save query image
+    img = decode_img(base64Img)
+    if not os.path.exists("data/uploaded/"): os.makedirs("data/uploaded/")
+    query_path = "data/uploaded/" + datetime.now().isoformat().replace(":", ".") + "_" + "query_img.png"
+    img.save(query_path)
 
-        base64Img = query_file['image']
-        x, y = int(float(query_file['x'])), int(float(query_file['y']))
-        x_max, y_max = int(float(query_file['x_max'])), int(float(query_file['y_max']))
-        
-        # Save query image
-        img = decode_img(base64Img)
-        if not os.path.exists("data/uploaded/"): os.makedirs("data/uploaded/")
-        query_path = "data/uploaded/" + datetime.now().isoformat().replace(":", ".") + "_" + "query_img.png"
-        img.save(query_path)
+    # Run search
+    if x1 == 0 and y1 == 0 and x2 == 0 and y2 == 0: x2, y2 = img.size
+    bbx = (x1, y1, x2, y2)
 
-        # Run search
-        if x == 0 and y == 0 and x_max == 0 and y_max == 0: x_max, y_max = img.size
-        bbx = (x, y, x_max, y_max)
+    if methodRequest == 0:
+        results = method_0(query_path, bbx, fe_method0, model)
+    elif methodRequest == 1:
+        results = method_1(query_path, bbx, fe_method1)
+    else:
+        results = method_2(query_path, [x1, y1, x2, y2], fe_method2, delf, 30)
 
-        results = method_0(query_path, bbx, method0, model)
-        results = [encode_img(str(path_corpus + i)) for i in results]
-        response = {'results': results}
-        
-        return json.dumps(response)
-    else: return render_template('Ups somthing went wrong!!!')
+    results = [encode_img(str(path_corpus + i)) for i in results]
+    response = {'results': results}
+    
+    return json.dumps(response)
 
 
 if __name__=="__main__":
-    # CORS(app)
-    # run_with_ngrok(app)
-    # print('ngrok')
     app.run()
