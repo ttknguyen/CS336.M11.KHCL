9da8fcb817da92f0191ff468353724040f9e8857
JD981 <19520235@gm.uit.edu.vn>
2022-02-03 10:01:58 +0700

    modify app.py, service.js & package.json

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import base64
 import io
+import os
 import json
 import numpy as np
 from PIL import Image
@@ -11,6 +12,8 @@ from flask_ngrok import run_with_ngrok
 from retrieval_model import load_features, load_corpus, method_0, method_1, method_2, load_methods
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 # Read image features
 root = '/content/CS336.M11.KHCL/'
@@ -19,7 +22,7 @@ path_corpus = '/content/CS336.M11.KHCL/data/test/oxford5k/jpg/'
 
 corpus = load_corpus(path_corpus)
 method0, method1, method2 = load_features(path_data, corpus)
-model, net, transform, ms, delf = load_methods(root)
+model, delf = load_methods(root)
 
 
 def decode_img(msg):
@@ -36,18 +39,20 @@ def encode_img(pathImg):
     return base64str
 
 @app.route('/', methods=['GET', 'POST'])
-@cross_origin(origin='*')
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def index():
     if request.method == 'POST':
         # Upload image
-        # file = request.files['query_img']
         query_file = request.form
+        # print(query_file)
+
         base64Img = query_file['image']
-        x, y = query_file['x'], query_file['y']
-        x_max, y_max = query_file['x_max'], query_file['y_max']
+        x, y = int(float(query_file['x'])), int(float(query_file['y']))
+        x_max, y_max = int(float(query_file['x_max'])), int(float(query_file['y_max']))
         
         # Save query image
         img = decode_img(base64Img)
+        if not os.path.exists("data/uploaded/"): os.makedirs("data/uploaded/")
         query_path = "data/uploaded/" + datetime.now().isoformat().replace(":", ".") + "_" + "query_img.png"
         img.save(query_path)
 
@@ -60,9 +65,7 @@ def index():
         response = {'results': results}
         
         return json.dumps(response)
-        
-    else:
-        return render_template('index.html')
+    else: return render_template('Ups somthing went wrong!!!')
 
 
 if __name__=="__main__":
